070ada07d3f3ec7777cc1cbc9db655de534f064e
Ujjawal Kumar <ujjawalkumar1810@gmail.com>
2019-10-05 17:22:27 +0530

    updated

---
+++ Program Practice/Program 1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int sum;
+
+	cout << "Enter a number :";
+	cin >> a;
+	cout << "Enter another number :";
+	cin >> b;
+
+	sum = a + b;
+
+	cout << "Sum of those Numbers is : " << sum;
+
+
+	return 0;
+}
--- Test1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-  void main()
-  {
-    int a;
-    a = 10;
-    printf("%d\n", a);
-  }
