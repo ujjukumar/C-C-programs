a7e4a264a17a0b1946f3e77d6efc714dcf304c1a
Ujjawal Kumar <ujjawalkumar1810@gmail.com>
2019-10-21 21:25:20 +0530

    Updated after configuring VS Code

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{   
+    char x[10];
+    cout<< "Hello I am new \n\n" << endl;
+    cout<< "Enter your name" << endl;
+    cin>> x  ;
+    cout<< x << endl;
+    system("pause");
+    return 0;
+}
