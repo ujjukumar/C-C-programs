09f07825bd9b472ea85c8b886a427ec717a17d30
Ujjawal Kumar <ujjawalkumar1810@gmail.com>
2019-10-25 17:31:59 +0530

    variables in a Class

---
+++ Classes with variables/variables_in_a_class.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Info {
+    
+    string name,col_name;
+    int age;
+
+    public:
+        void setValues(string x, string y, int z) {
+            name = x;
+            col_name = y;
+            age = z;
+        }
+
+        string getNames() {
+            cout << "Name : " << name;
+            return  cout << "\nCollege name : ", col_name;
+        }
+
+        int getAge() {
+            cout << "\nAge : ";
+            return age;
+        }
+};
+
+int main() {
+
+    Info Ujju;
+    Ujju.setValues("Ujjawal Kumar", "Dayananda Sagar College of Engineering", 20);
+    cout << "\n\nThe Details of the person is as follows\n\n";
+    cout << Ujju.getNames();
+    cout << Ujju.getAge() << "\n\n" << endl;
+
+    return 0;
+}
