6bde9c52a1daeb4cf41b8adbe107ed7e387a9f8e
Ujjawal Kumar <ujjawalkumar1810@gmail.com>
2019-11-06 22:27:44 +0530

    removed extra files

--- classProject/derivedClasses.cpp
+++ classProject/derivedClasses.cpp
@@ -1,7 +1,22 @@
-// #include "baseClassGates.cpp"
+#include "baseClassGates.cpp"
 
-// class halfAdder : public Gates {
-//     int input1, input2;
+class halfAdder : public Gates {
 
+    public:
+    
+        int operation(int a, int b, int sum,int carry) {
+            sum = xorFunction(a, b);
+            carry = andFunction(a, b);
+        }
 
-// }
+        int truthTable() {
+
+            cout << "-----TRUTH TABLE-----\n\n" << endl;
+            cout << "A\t\t" << "B\t\t\t" << "SUM\t\tCARRY" <<endl;
+            cout << "0\t\t" << "0\t\t\t" << "0\t\t0" <<endl;
+            cout << "0\t\t" << "1\t\t\t" << "1\t\t0" <<endl;
+            cout << "1\t\t" << "0\t\t\t" << "1\t\t0" <<endl;
+            cout << "1\t\t" << "1\t\t\t" << "1\t\t1" <<endl;
+            cout << "------------------------" << endl;
+        }
+};
